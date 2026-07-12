c08d3996974b01217a9950e1e09fe84ff931440b
Ankur Upadhyay <ankur.upadhyay7v@gmail.com>
2026-07-12 09:46:58 +0530

    Setup GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   // Required since Capacitor loads the static export from file:// which means we can't use Next.js Image Optimization
   images: {
     unoptimized: true,
